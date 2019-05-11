0252fa4870ccd68df957f573d2bff521da50dd6b
zrobin88 <zacharyrobin@zacharys-mbp-2.home>
2019-05-11 16:16:00 -0400

    added more videos

--- client/src/components/Alist-Header/index.js
+++ client/src/components/Alist-Header/index.js
@@ -4,10 +4,15 @@ import greenMatrix from "../../video/greenMatrix.mp4";
 import colorLights from "../../video/Colorful Lights.mp4";
 import geetar from "../../video/geetar.mp4";
 import blueLight from "../../video/blueLight.mp4";
-import record from "../../video/record.mp4"
+import record from "../../video/record.mp4";
+import neon from "../../video/Neon - 21368.mp4";
+import dj from"../../video/dj.mp4";
+import rc from"../../video/rc.mp4.mp4";
+import fender from "../../video/fender.mp4";
+import tape from "../../video/tape.mp4";
+import tt from "../../video/Turntable - 10857.mp4"
 
-
-let videoArr = [greenMatrix, colorLights, geetar, blueLight, record];
+let videoArr = [greenMatrix, colorLights, geetar ,dj, tape, tt, rc, fender, blueLight, neon, record];
 let video = videoArr[Math.floor(Math.random()*videoArr.length)];
 
 function AlistHeader(props) {
